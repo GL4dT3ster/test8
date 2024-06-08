aaa91b3f7d5820cc353334e94417d8424eca6a95
GL4dT3ster <gl4dt3ster@users.noreply.github.com>
2024-06-08 12:26:53 +0400

    .

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 3 numbers, I'll find the greatest: ");
+        System.out.print("1) ");
+        float num1 = scanner.nextFloat();
+        System.out.print("2) ");
+        float num2 = scanner.nextFloat();
+        System.out.print("3) ");
+        float num3 = scanner.nextFloat();
+
+        if (num1 > num2 && num2 >= num3){
+            System.out.print("The greatest number is " + num1);
+        } else if (num1 > num2 && num3 >= num2 && num1 > num3){
+            System.out.print("The greatest number is " + num1);
+        } else if (num2 > num1 && num1 >= num3){
+            System.out.print("The greatest number is " + num2);
+        } else if (num2 > num1 && num3 >= num1 && num2 > num3){
+            System.out.print("The greatest number is " + num2);
+        } else if (num3 > num2 && num2 >= num1){
+            System.out.print("The greatest number is " + num3);
+        } else if (num3 > num2 && num1 >= num2 && num3 > num1) {
+            System.out.print("The greatest number is " + num3);
+        } else if (num1 == num2 && num2 > num3){
+            System.out.print("The first and the second numbers are the same. The greatest number is " + num1);
+        } else if (num3 == num2 && num2 > num1){
+            System.out.print("The second and the third numbers are the same. The greatest number is " + num2);
+        } else if (num1 == num3 && num3 > num2){
+            System.out.print("The first and the third numbers are the same. The greatest number is " + num1);
+        } else {
+            System.out.print("Numbers are the same. There is no the greatest number");
         }
     }
 }
